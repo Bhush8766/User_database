baf03ce2db3c7f4a639728788fc2166aa71a2ce4
Bhush_Ahire <159047235+Bhush8766@users.noreply.github.com>
2025-07-20 22:05:37 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,26 +1,12 @@
+const mongoose = require("mongoose");
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://bhush8766:Bhush%402328%40@cluster0.yawkujz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.set("strictQuery", false);
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+setTimeout(() => {
+  mongoose.connect("mongodb+srv://bhush8766:Bhush%402328@cluster0.yawkujz.mongodb.net/userData?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+}, 3000);
