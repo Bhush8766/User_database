bc3db004538a42fdf791fe22a5b283e85e522e33
Bhush_Ahire <159047235+Bhush8766@users.noreply.github.com>
2025-07-20 20:30:06 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongoose.connect("mongoose.connect("mongodb+srv://bhush123:myP%40ssword123@cluster0.mongodb.net/userData?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://bhush123:myP%40ssword123@cluster0.mongodb.net/userData?retryWrites=true&w=majority");
 
 const userSchema = mongoose.Schema({
     image: String,
